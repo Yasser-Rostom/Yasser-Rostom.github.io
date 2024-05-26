fdcb9c300ae416d89aaf4708c22af46a742cb957
yasser9340@gmail.com <2>
2024-05-26 09:51:21 +0400

    updated assets paths

--- lib/app/utils/project_utils.dart
+++ lib/app/utils/project_utils.dart
@@ -17,21 +17,21 @@ class ProjectUtils {
 List<ProjectUtils> projectUtils = [
   ProjectUtils(
       title: "Cryptonaire",
-      imgUrl: "projects/cryptonaire_app.png",
+      imgUrl: "assets/projects/cryptonaire_app.png",
       projectDesc:
           "Cryptonaire is an exciting new quiz & gaming app that lets users learn about new crypto projects and earn rewards in the form of cryptocurrencies.",
       stack: "Flutter, Firebase Notification, Bloc, MongoDB, Figma",
       projectUrl: "https://cryptonaire.app/"),
   ProjectUtils(
       title: "CryptoChefs - UI/UX Design",
-      imgUrl: "projects/cheesy_wheel.png",
+      imgUrl: "assets/projects/cheesy_wheel.png",
       projectDesc:
           "Complete design of a new feature called Bistro",
       stack: "Figma, Photoshop",
       projectUrl: "https://app.cryptochefs.io/bistro"),
   ProjectUtils(
       title: "Balanced News Summary",
-      imgUrl: "projects/bns.jpeg",
+      imgUrl: "assets/projects/bns.jpeg",
       projectDesc:
           "BNS is a platform allows readers to read articles and rate their bias.",
       stack: "Flutter, Firebase DB",
@@ -39,7 +39,7 @@ List<ProjectUtils> projectUtils = [
           "https://play.google.com/store/apps/details?id=com.aus.balancednews"),
   ProjectUtils(
       title: "Scam Scan",
-      imgUrl: "projects/scamscan.png",
+      imgUrl: "assets/projects/scamscan.png",
       projectDesc:
           "ScamScan is an app for travellers by travellers, designed to help you to reaching your destination – safely. No matter if its a business-, honeymoon or adventure trip.",
       stack: "Kotlin, Xml, Firebase Realtime DB",
@@ -47,7 +47,7 @@ List<ProjectUtils> projectUtils = [
           "https://play.google.com/store/apps/details?id=com.geo.scamscan"),
   ProjectUtils(
       title: "Auditory Advantage - TTS",
-      imgUrl: "projects/auditory_advantage.png",
+      imgUrl: "assets/projects/auditory_advantage.png",
       projectDesc:
           "Auditory Advantage is an auto reader and voice speaker that acts as a PDF reader, book reader, and image reader.",
       stack: "Java, Xml, Local DB",
@@ -55,7 +55,7 @@ List<ProjectUtils> projectUtils = [
           "https://play.google.com/store/apps/details?id=com.aus.balancednews"),
   ProjectUtils(
       title: "Solo Mas App | Bug Fixes and Ads integration",
-      imgUrl: "projects/solomas.jpeg",
+      imgUrl: "assets/projects/solomas.jpeg",
       projectDesc:
           "The Solo Mas App is a social travel app which connects its users with Carnival enthusiasts and masqueraders near them. I have helped with bug fixes and integrated custom-designed ads in non-intrusive ways",
       stack: "Flutter, BLoC, Admob, Unit Testing, Figma",
--- lib/app/utils/service_utils.dart
+++ lib/app/utils/service_utils.dart
@@ -15,21 +15,21 @@ class ServiceUtils {
 List<ServiceUtils> serviceUtils = [
   ServiceUtils(
     title: "Mobile Development",
-    imgUrl: "services/programming.png",
+    imgUrl: "assets/services/programming.png",
     projectDesc:
         "State-of-the-art mobile app development for both Android & iOS.",
     stack: "Flutter, Java, and Kotlin",
   ),
   ServiceUtils(
     title: "Web Development",
-    imgUrl: "services/web_dev.png",
+    imgUrl: "assets/services/web_dev.png",
     projectDesc:
     "Responsive, light-weight web application.",
     stack: "Flutter, Figma, HTML, CSS",
   ),
   ServiceUtils(
     title: "Mobile UI/UX Design",
-    imgUrl: "services/designing.png",
+    imgUrl: "assets/services/designing.png",
     projectDesc:
         "Eye-catching, user-friendly UIs following the best practices.",
     stack: "Figma, Adobe XD",
