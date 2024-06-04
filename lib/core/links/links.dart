154e39c3efad5a838db7df60a91a4160b319bf8d
yasser9340@gmail.com <2>
2024-06-04 16:55:28 +0400

    cv link change

--- lib/core/links/links.dart
+++ lib/core/links/links.dart
@@ -1,6 +1,6 @@
 const String gitHub = 'https://github.com/yasser-rostom';
 const String upworkProfile = 'https://www.upwork.com/freelancers/~01cc61b694f881b170';
 const String linkedin = 'https://www.linkedin.com/in/yasser-rostom-8b66a1121/';
-const String cv = 'https://drive.google.com/file/d/1EuwwReK5KLNP3uYEOUkXEWR6hcvxcVsO/view';
+const String cv = 'https://docs.google.com/document/d/1mjyNqxL-aa80_XRYCpoVNuObAsVxdoyC/edit?usp=sharing&ouid=106230380821989909115&rtpof=true&sd=true';
 const String whatsapp = 'https://api.whatsapp.com/send?phone=971529979983';
 const String twitter = 'https://x.com/yasser9340';
